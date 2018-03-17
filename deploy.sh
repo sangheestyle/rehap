94adfaaa7325ad290f6d81aec253b5b2fb34d466
Sanghee Kim <sangheestyle@gmail.com>
2018-03-17 13:36:04 -0400

    ugly deployment script
    TODO: need to make it better.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker-compose up -d
+docker commit rehap_nginx_1 rehap_nginx
+docker tag rehap_nginx sangheestyle/rehap_nginx
+docker push sangheestyle/rehap_nginx
+
+docker commit rehap_appsvr_1 rehap_appsvr
+docker tag rehap_appsvr sangheestyle/rehap_appsvr
+docker push sangheestyle/rehap_appsvr
+
+docker-compose stop
+
+eb deploy
